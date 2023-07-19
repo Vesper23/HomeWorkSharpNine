e04f983a34441f09b2d690814ebc0a5c36a85720
Valentin Likholat <phantom.231220@gmail.com>
2023-07-19 22:09:08 +0300

    Completed 1 Task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте значение N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+
+int InputNum(string message) //Ввод с клавиатуры
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void PrintNumbers(int start, int end) //Вывод натуральных чисел от N до 1
+{
+  if (end > start) return;
+  PrintNumbers(start, end + 1);
+  Console.Write(end + ", ");
+}
+
+int n = InputNum("Введите N: ");
+int count = 2;
+PrintNumbers(n, count);
+Console.Write(1);
