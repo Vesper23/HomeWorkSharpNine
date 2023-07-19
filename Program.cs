a0fa161f098304c80063ecd97ce2cb2f4570f382
Valentin Likholat <phantom.231220@gmail.com>
2023-07-19 22:32:57 +0300

    Completed 3 Task

--- Program.cs
+++ Program.cs
@@ -22,9 +22,12 @@ PrintNumbers(n, count);
 Console.Write(1);
 */
 
+//------------------------------------------------------------------------------------------------------------------------------------
+
 //Задайте значения M и N. 
 //Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
+/*
 int InputNum(string message) //Ввод с клавиатуры
 {
     Console.Write(message);
@@ -53,3 +56,29 @@ if (m > n)
 }
 
 PrintSumm(m, n, local=0);
+*/
+
+//------------------------------------------------------------------------------------------------------------------------------------
+
+//Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+
+int InputNum(string message) //Ввод с клавиатуры
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int AkkerFunc(int m, int n) //Расчет функции Аккермана
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return AkkerFunc(m - 1, 1);
+  else return AkkerFunc(m - 1, AkkerFunc(m, n - 1));
+}
+
+int m = InputNum("Введите M: ");
+int n = InputNum("Введите N: ");
+
+int functionAkkerman = AkkerFunc(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
