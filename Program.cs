01df68c59af35ba1f4473a2c90e1a5c65932f465
Valentin Likholat <phantom.231220@gmail.com>
2023-07-19 22:15:53 +0300

    Completed 2 Task

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 //Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
 //Выполнить с помощью рекурсии.
 
+/*
 int InputNum(string message) //Ввод с клавиатуры
 {
     Console.Write(message);
@@ -19,3 +20,36 @@ int n = InputNum("Введите N: ");
 int count = 2;
 PrintNumbers(n, count);
 Console.Write(1);
+*/
+
+//Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int InputNum(string message) //Ввод с клавиатуры
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void PrintSumm(int m, int n, int summ) //Сумма элементов
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов = {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+
+int m = InputNum("Введите M: ");
+int n = InputNum("Введите N: ");
+int local = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = local;
+}
+
+PrintSumm(m, n, local=0);
